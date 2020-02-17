a43d202d716ca7d61486b374cd6b78b58eb4033b
dawiddominiak <poczta@dawiddominiak.pl>
2020-02-17 14:11:45 +0100

    Make app wokring

--- src/score/score.module.ts
+++ src/score/score.module.ts
@@ -1,14 +1,21 @@
+import { UserModule } from 'src/user/user.module';
+
 import { Module } from '@nestjs/common';
 
 import { ScoreController } from './score.controller';
 import { ScoreMapper } from './score.mapper';
+import { ScoreService } from './score.service';
 
 @Module({
   providers: [
     ScoreMapper,
+    ScoreService,
   ],
   controllers: [
     ScoreController,
+  ],
+  imports: [
+    UserModule
   ]
 })
 export class ScoreModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -10,6 +10,7 @@ import { UserService } from './user.service';
     TypeOrmModule.forFeature([UserRepository]),
   ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService]
 })
 export class UserModule {}
