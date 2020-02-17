ef8325365ddcf7253adb9c96608aaad2fc713c46
dawiddominiak <poczta@dawiddominiak.pl>
2020-02-17 18:23:37 +0100

    Postgresql version

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -6,11 +6,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     ConfigModule,
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: ':memory:',
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
       synchronize: true,
-      keepConnectionAlive: process.env.NODE_ENV === 'test',
     }),
   ],
 })
